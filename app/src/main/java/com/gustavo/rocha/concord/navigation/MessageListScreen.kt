ae9370d43c3f412b9d0b531c4a795c48fb0c43ea
gustavo rocha <pereiragustavo215@gmail.com>
2023-06-18 14:39:11 -0300

    Utilizando seletor de arquivos

--- app/src/main/java/com/gustavo/rocha/concord/navigation/MessageListScreen.kt
+++ app/src/main/java/com/gustavo/rocha/concord/navigation/MessageListScreen.kt
@@ -1,7 +1,9 @@
 package com.gustavo.rocha.concord.navigation
 
+import android.provider.OpenableColumns
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -74,6 +76,24 @@ fun NavGraphBuilder.messageListScreen(onBack: () -> Unit = {}) {
                 }
             }
 
+            val pickFile = rememberLauncherForActivityResult(
+                ActivityResultContracts.OpenDocument()
+            ) { uri ->
+                if (uri != null) {
+
+                    val name = context.contentResolver
+                        .query(uri, null, null, null, null).use { cursor ->
+                            val nameIndex = cursor?.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                            cursor?.moveToFirst()
+                            nameIndex?.let { cursor.getString(it) }
+                        }
+
+                    uiState.onMessageValueChange(name.toString())
+                    viewModelMessage.loadMediaInScreen(uri.toString())
+                    viewModelMessage.sendMessage()
+                }
+            }
+
             if (uiState.showBottomSheetFile) {
                 ModalBottomSheetFile(
                     onSelectPhoto = {
@@ -81,6 +101,9 @@ fun NavGraphBuilder.messageListScreen(onBack: () -> Unit = {}) {
                         viewModelMessage.setShowBottomSheetFile(false)
                     },
                     onSelectFile = {
+                        // */* para buscar todos os arquivos
+                        // "image/pgn", "application/pdf"
+                        pickFile.launch(arrayOf("*/*"))
                         viewModelMessage.setShowBottomSheetFile(false)
                     }, onBack = {
                         viewModelMessage.setShowBottomSheetFile(false)
